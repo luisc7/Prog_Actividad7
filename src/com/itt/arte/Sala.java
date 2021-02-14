49c1fefeb20a44b860013d13cab5edabd33f5fa7
Luis Cifuentes <luiscifuentes7@gmail.com>
2021-02-15 00:49:12 +0100

    Hecha interfaz Sala

--- src/com/itt/arte/Sala.java
+++ src/com/itt/arte/Sala.java
@@ -1,5 +1,12 @@
 package com.itt.arte;
 
-public class Sala {
-
+public interface Sala {
+	
+	public String verProgramacion();
+	public String verLocalidades();
+	public String verLocalidadesOcupadas();
+	public String venderLocalidad(int fila, int butaca, Espectador e);
+	public String cancelarLocalidad(int fila, int butaca);
+	public String consultarLocalidad(int fila, int butaca);
+	public double calcularRecaudacion();
 }

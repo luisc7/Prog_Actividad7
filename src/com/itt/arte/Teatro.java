9637764936a94050764e924869b530009507c501
Luis Cifuentes <luiscifuentes7@gmail.com>
2021-02-16 00:41:12 +0100

    Cambios en Teatro y main para opcion 4 de main (vender localidad)

--- src/com/itt/arte/Teatro.java
+++ src/com/itt/arte/Teatro.java
@@ -1,22 +1,28 @@
 package com.itt.arte;
-//import java.lang.Object;
 
 public class Teatro extends Local implements Sala{
 	private Obra obra;
 	private double precio;
 	private Espectador[][] localidades;
 	
-	private static final int FILAS = 5;
-	private static final int COLUMNAS = 10;
+	private int filass;
+	private int columnass;
 	
+		
 	// Constructores
 	public Teatro(String domi, int metr, int acce, Obra ob, double pre) {
 		super(domi, metr, acce);
 		this.obra = ob;
-		//this.localidades = new Espectador[FILAS][COLUMNAS];
-		this.localidades = new Espectador[FILAS][COLUMNAS];
-		for (int i=0; i<FILAS; i++) {
-			for (int j=0; j<COLUMNAS; j++) {
+		
+		final int FILAS = 5;
+		final int COLUMNAS = 10;
+		
+		this.filass = FILAS;
+		this.columnass = COLUMNAS;
+		
+		this.localidades = new Espectador[filass][columnass];
+		for (int i=0; i<filass; i++) {
+			for (int j=0; j<columnass; j++) {
 				 localidades[i][j] = new Espectador();
 			}
 		}
@@ -35,9 +41,9 @@ public class Teatro extends Local implements Sala{
 	}
 	public String verLocalidades() {
 		StringBuilder lista = new StringBuilder("");
-		for (int i=0; i<FILAS; i++) {
+		for (int i=0; i<filass; i++) {
 			lista.append("\n");
-			for (int j=0; j<COLUMNAS; j++) {
+			for (int j=0; j<columnass; j++) {
 				lista.append(i + "." + j + " ");
 				if (localidades[i][j].getEdad() == -1) {
 					lista.append("Libre\t");
@@ -51,8 +57,8 @@ public class Teatro extends Local implements Sala{
 	
 	public String verLocalidadesOcupadas() {
 		StringBuilder ocupados = new StringBuilder("");
-		for (int i=0; i<FILAS; i++) {
-			for (int j=0; j<COLUMNAS; j++) {
+		for (int i=0; i<filass; i++) {
+			for (int j=0; j<columnass; j++) {
 				 if ((localidades[i][j].getEdad()) != (-1)) {
 					 ocupados.append("\n");
 					 ocupados.append(i + "." + j + " ");
@@ -65,6 +71,8 @@ public class Teatro extends Local implements Sala{
 		return ocupados.toString();
 	}
 	public String venderLocalidad(int fila, int butaca, Espectador e) {
+	    
+		
 		return "";
 	}
 	public String cancelarLocalidad(int fila, int butaca) {
@@ -75,6 +83,27 @@ public class Teatro extends Local implements Sala{
 	}
 	public double calcularRecaudacion() {
 		return 0;
+	}
+	
+	//getters y setters
+	public int getFilass() {
+		return this.filass;
+	}
+	
+	public int getColumnass() {
+		return this.columnass;
+	}
+	
+	public double getPrecio() {
+		return this.precio;
+	}
+	
+	public Espectador getLocalidades(int f, int c){
+		return this.localidades[f][c];
+	}
+	
+	public void setLocalidades(int f, int c, String nom, String tel, int edd){
+		localidades[f][c] = new Espectador(nom, tel, edd);
 	}
 	
 
--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -5,21 +5,74 @@ import com.itt.arte.*;
 
 public class Principal {
 	private static Scanner lector;
+	private static Scanner numteclado;
+	private static Scanner teclado;
+	
 	private static Obra obra = new Obra("La cena de los idiotas", "Comedia", 95);
 	private static Teatro teatro = new Teatro("C/ Sol, 45", 300, 2, obra, 30);
+	
 	public static void main(String[] args) {
 		lector = new Scanner(System.in);
 		char opc;
 		do {
 			opc = mostrarMenu();
+			
 			switch (opc) {
+			
 				case '1':
 					System.out.println(teatro.verProgramacion());
+					
 				case '2':
 					System.out.println(teatro.verLocalidades());
+				
 				case '3':
 					System.out.println(teatro.verLocalidadesOcupadas());
+				
 				case '4':
+					numteclado = new Scanner(System.in);
+					teclado = new Scanner(System.in);
+					
+				    int fil = 0;
+				    int col = 0;
+				    int eda = 0;
+				    
+				    double dto = 0;
+				    double precdto = 0;
+										
+					System.out.println("Elija la fila en que prefiera sentarse (de 0 a " + teatro.getFilass());
+					fil = numteclado.nextInt();
+					System.out.println("Elija la butaca (de 0 a " + teatro.getColumnass());
+					col = numteclado.nextInt();
+					
+					if (teatro.getLocalidades(fil, col).getEdad() >= 0) {
+						System.out.println("Lo sentimos, esa butaca está ocupada.");
+						
+					} else {
+						System.out.println("Indique su nombre:");
+						String nombre = teclado.nextLine();
+						System.out.println("Introduzca su telefono:");
+						String telefono = teclado.nextLine();
+						System.out.println("Ponga su edad:");
+						eda = numteclado.nextInt();
+						
+						if (0 <= eda && eda < 13) {
+							dto = 0.5;
+						} else if (eda >= 13 && eda < 18) {
+							dto = 0.2;
+						} else if (eda >= 65) {
+							dto = 0.66;
+						} else {
+							dto = 0;
+						}
+						
+						precdto = teatro.getPrecio() * (1 - dto);
+						
+						teatro.setLocalidades(fil, col, nombre, telefono, eda);
+						System.out.println("Se ha vendido la localidad "+ fil + "." + col + 
+								" a " + nombre + " por " + precdto + " Euros");
+					
+					}
+				
 				case '5':
 				case '6':
 				case '7':

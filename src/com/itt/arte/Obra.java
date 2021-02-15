b1a6cb11b7b10a657b1c9a04bf509dc4b3662fec
Luis Cifuentes <luiscifuentes7@gmail.com>
2021-02-15 01:59:27 +0100

    Clase Obra sin toString

--- src/com/itt/arte/Obra.java
+++ src/com/itt/arte/Obra.java
@@ -1,5 +1,44 @@
 package com.itt.arte;
 
 public class Obra {
+	private String titulo;
+	private String genero;
+	private int minutosDuracion;
+	
+	public Obra(String tit, String gen, int dur) {
+		this.titulo = tit;
+		this.genero = gen;
+		this.minutosDuracion = dur;
+	}
 
+	// Falta perfilar el toString para el uso final
+	@Override
+	public String toString() {
+		return "";
+	}
+	
+	// getters y setters
+	public String getTitulo() {
+		return this.titulo;
+	}
+	
+	public String getGenero() {
+		return this.genero;
+	}
+	
+	public int setMinutosDuracion() {
+		return this.minutosDuracion;
+	}
+	
+	public void setTitulo(String tit) {
+		this.titulo = tit;
+	}
+	
+	public void setGenero(String gen) {
+		this.genero = gen;
+	}
+	
+	public void setDuracion(int dur) {
+		this.minutosDuracion = dur;
+	}
 }
